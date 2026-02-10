60c95a1e37520ca35a17796c2dbb7782dc410de0
João Manoel <jmtac783@gmail.com>
2026-02-10 19:57:06 -0300

    update

---
+++ fundamentos/4°-condicoes/ex9.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+
+if (pais == 'brasil') {
+    console.log(`você nasceu no ${pais} então você é brasileiro`)
+}   else {
+    console.log(`você nasceu no/a ${pais} então você é estrangeiro`)
+}
