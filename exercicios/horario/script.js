8c0836043c4485c3ef1d218c77172cddda21b365
João Manoel <jmtac783@gmail.com>
2026-02-11 18:34:23 -0300

    update

--- exercicios/horario/script.js
+++ exercicios/horario/script.js
@@ -1,19 +1,19 @@
-var agora = new Date()
-var hora = agora.getHours()
-var body = document.body
-var msg = document.getElementById('msg')
-var img = document.getElementById('img')
+function carregar() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var body = document.body
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    msg.innerHTML = `agora são ${hora} horas.`
 
-if (hora < 12) {
-    msg.innerHTML = `agora são ${hora} horas.`
-    body.style.backgroundColor = 'rgb(194, 194, 95)'
-    img.src = 'imagens/bom_dia.jpg'
-} else if (hora < 18) {
-    msg.innerHTML = `agora são ${hora} horas.`
-    body.style.backgroundColor = 'rgb(245, 141, 84)'
-    img.src = 'imagens/boa_tarde.jpg'
-} else {
-    msg.innerHTML = `agora são ${hora} horas.`
-    body.style.backgroundColor = 'rgb(21, 21, 67)'
-    img.src = 'imagens/boa_noite.jpg'
+    if (hora < 12) {
+        body.style.backgroundColor = 'rgb(194, 194, 95)'
+        img.src = 'imagens/bom_dia.jpg'
+    } else if (hora < 19) {
+        body.style.backgroundColor = 'rgb(245, 141, 84)'
+        img.src = 'imagens/boa_tarde.jpg'
+    } else {
+        body.style.backgroundColor = 'rgb(48, 48, 63)'
+        img.src = 'imagens/boa_noite.jpg'
+    }
 }
--- exercicios/verificador_idade/script.js
+++ exercicios/verificador_idade/script.js
@@ -2,35 +2,37 @@ function verificar() {
     var anotxt = document.getElementById('ano')
     var ano = Number(anotxt.value)
     var agora = new Date()
-    var idade = agora.getFullYear() - ano
-    var sexo = document.querySelector('input[name="sexo"]:checked')
     var msg = document.getElementById('msg')
     var img = document.getElementById('img')
-    
-
+    if (ano == 0 || ano > agora.getFullYear()) {
+        window.alert('verifique se os dados estão corretos')
+    } else {
+        var idade = agora.getFullYear() - ano
+        var sexo = document.querySelector('input[name="sexo"]:checked')
+    }
     if (idade < 13 && sexo.value == 'm') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `criança de ${idade} anos` 
         img.src = 'imagens/criança_m.jpg'
     } else if (idade < 13 && sexo.value == 'f') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `criança de ${idade} anos` 
         img.src = 'imagens/criança_f.jpg'
     } else if (idade < 18 && sexo.value == 'm') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `adolecente de ${idade} anos` 
         img.src = 'imagens/adolecente_m.jpg'
     } else if (idade < 18 && sexo.value == 'f') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `adolecente de ${idade} anos` 
         img.src = 'imagens/adolecente_f.jpg'
     } else if (idade < 60 && sexo.value == 'm') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `adulto de ${idade} anos` 
         img.src = 'imagens/adulto_m.jpg'
     } else if (idade < 60 && sexo.value == 'f') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `adulto de ${idade} anos` 
         img.src = 'imagens/adulto_f.jpg'
     } else if (idade >= 60 && sexo.value == 'm') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `idoso de ${idade} anos` 
         img.src = 'imagens/idoso_m.jpg'
     } else if (idade >= 60 && sexo.value == 'f') {
-        msg.innerHTML = `pessoa de ${idade} anos` 
+        msg.innerHTML = `idoso de ${idade} anos` 
         img.src = 'imagens/idoso_f.jpg'
     }
 }
