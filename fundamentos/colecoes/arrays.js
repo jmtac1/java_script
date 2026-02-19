13772bbe513bdf58e37d5315b2702e966351f72b
João Manoel <jmtac783@gmail.com>
2026-02-19 19:02:41 -0300

    update

--- fundamentos/colecoes/arrays.js
+++ fundamentos/colecoes/arrays.js
@@ -1,7 +1,10 @@
 var num = [5, 6, 1, 3, 7]
-num.push(4)
-num.sort()
-num.pop()
+num.push(4) // adiciona elemento no fim do array
+num.sort() // organiza os elementos em ordem numerica ou alfabetica
+num.sort().reverse() // organiza os elementos do ultimo para o primeiro
+num.pop() // apaga o ultimo elemento
+num.unshift(8) // adiciona elemento no inicio do array
+num.shift() // remove elemento do inicio do array
 
 console.log(num.indexOf(8))
 console.log(num.length)
