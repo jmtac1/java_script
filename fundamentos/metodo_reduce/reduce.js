9f04dfffe068059cbc24b8be60c2bf64a9276cb0
João Manoel <jmtac783@gmail.com>
2026-02-20 18:41:15 -0300

    Create reduce.js

---
+++ fundamentos/metodo_reduce/reduce.js
@@ -0,0 +1,21 @@
+// O método reduce() no JavaScript é uma função de ordem superior que percorre um array para reduzi-lo a um único valor final 
+
+// exemplo 1
+const preços = [10, 15, 3, 2.50, 5.85, 0.65];
+const total = preços.reduce(soma);
+
+console.log(`R$: ${total.toFixed(2)}`);
+
+function soma(anterior, proximo) {
+    return anterior + proximo;
+}
+
+// exemplo 2
+const notas = [5, 4, 7, 8, 10, 2];
+const maximo = notas.reduce(max)
+
+console.log(maximo);
+
+function max(anterior, proximo) {
+    return Math.max(anterior, proximo);
+}
