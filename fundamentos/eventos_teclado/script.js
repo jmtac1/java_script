ff13c6839ab8a1871fd48a42a1c2ebc5275fc775
João Manoel <jmtac783@gmail.com>
2026-02-27 19:52:47 -0300

    update

--- fundamentos/eventos_teclado/script.js
+++ fundamentos/eventos_teclado/script.js
@@ -1,7 +1,33 @@
+const caixa = document.getElementById('caixa');
+const movimento = 10;
+let x = 0;
+let y = 0;
+
 document.addEventListener('keydown', evento => {
-    document.body.style.backgroundColor = 'seagreen';
+    if (evento.key.startsWith('Arrow')) {
+        switch (evento.key) {
+            case 'ArrowUp':
+                y -= movimento;
+                break;
+            case 'ArrowDown':
+                y += movimento;
+                break;
+            case 'ArrowLeft':
+                x -= movimento;
+                break;
+            case 'ArrowRight':
+                x += movimento;
+                break;
+        }
+
+        caixa.style.top = `${y}px`;
+        caixa.style.left = `${x}px`;
+    }
+    caixa.textContent = '😂';
+    caixa.style.backgroundColor = 'darkblue';
 });
 
 document.addEventListener('keyup', evento => {
-    document.body.style.backgroundColor = 'darkblue';
+    caixa.textContent = '😀';
+    caixa.style.backgroundColor = 'seagreen';
 })
